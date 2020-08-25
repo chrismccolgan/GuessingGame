3f13fa87eaec3a0aae590b273a996088a1578ff8
chrismccolgan <mccolganchrisj@gmail.com>
2020-08-25 11:30:23 -0500

    Completed Phase 3

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write("Guess the secret number: ");
+                string answer = Console.ReadLine();
+                int num = int.Parse(answer);
+                int secretNumber = 42;
+
+                if (num == secretNumber)
+                {
+                    Console.WriteLine("Congratulations!");
+                }
+                else if (num != secretNumber)
+                {
+                    Console.WriteLine("Nope");
+                }
+            }
+        }
+    }
+}
